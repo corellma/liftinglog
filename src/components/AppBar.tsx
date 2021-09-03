6dbea51e302b0f01e01d9005a7e45ada6de2625e
corellma <marcel.corell.94@gmail.com>
2021-09-03 07:59:15 +0000

    added simpe routing for NavBar items

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Link from '@material-ui/core/Link';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,16 +25,33 @@ export default function ButtonAppBar() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <Router>
+      <div className={classes.root}>
       <AppBar position='static'>
         <Toolbar>
-          <Typography variant='h6' className={classes.title}>
+          <Link variant='h6' color='inherit' className={classes.title} href='/'>
             Lifting Log
-          </Typography>
-          <Button color='inherit'>Dashboard</Button>
-          <Button color='inherit'>Lifts</Button>
+            </Link>
+          <Button color='inherit' href='/dashboard'>
+            Dashboard
+          </Button>
+          <Button color='inherit' href='/lifts'>Lifts</Button>
         </Toolbar>
       </AppBar>
     </div>
+
+    <Switch>
+      <Route path='/dashboard'>
+        Dashboard
+      </Route>
+      <Route path='/lifts'>
+        Lifts
+      </Route>
+      <Route path='/'>
+        Home
+      </Route>
+    </Switch>
+    </Router>
+    
   );
 }
