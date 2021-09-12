be21afbd67680633f2e05e264fa052c35f677a4f
corellma <marcel.corell.94@gmail.com>
2021-09-12 16:16:43 +0000

    added get_unique_lifts to db. how to work w/ promises?

--- src/components/lift_collection.tsx
+++ src/components/lift_collection.tsx
@@ -1,25 +1,8 @@
+import { IndexableTypeArray, PromiseExtended } from "dexie";
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from "../models/db";
 import { Lift } from "../models/lift";
 
-export default function Lift_Collection() {
-  const unique_lifts = useLiveQuery(() =>
-    db.lifts.orderBy("lift").uniqueKeys()
-  );
-  console.log(unique_lifts);
-
-  // For each unique lift, get all related db entries
-  
+export default function PR_Collection() {
   return null;
-
-  /*   if (!lifts) return null; // Still loading.
-  return (
-    <ul>
-      {lifts.map((lift) => (
-        <li key={lift.id}>
-          {lift.lift}, {lift.e1RM}
-        </li>
-      ))}
-    </ul>
-  ); */
 }
--- src/models/db.ts
+++ src/models/db.ts
@@ -1,4 +1,4 @@
-import Dexie from "dexie";
+import Dexie, { IndexableTypeArray, PromiseExtended } from "dexie";
 import { Lift } from "./lift";
 
 class LiftingLog extends Dexie {
@@ -10,6 +10,10 @@ class LiftingLog extends Dexie {
       lifts: "++id,lift,weight,reps,e1rm,date",
     });
     this.lifts = this.table("lifts");
+  }
+
+  get_unique_lifts(): Promise<IndexableTypeArray> {
+    return this.lifts.orderBy("lift").uniqueKeys();
   }
 }
 
